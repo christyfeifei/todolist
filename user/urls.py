af4459f6068badf9d293193aac17cd7601c76fbb
christy <vennesach@gmail.com>
2023-05-11 11:05:56 +0800

    完成登入畫面與檢查

--- user/urls.py
+++ user/urls.py
@@ -19,5 +19,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("",views.user_register,name='register')
+    path("",views.user_register,name='register'),
+    path("login/",views.user_login,name='login'),
 ]
