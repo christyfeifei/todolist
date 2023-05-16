460cf3f51ba6fb852d991fea0c98965fe4dd4edc
christy <vennesach@gmail.com>
2023-05-16 09:52:46 +0800

    建立Todo資料模型進行同步跟顯示動作

---
+++ todo/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from .models import Todo
+# Create your views here.
+
+def todo(request):
+    todos=Todo.objects.all()
+    return render(request,'todo/todo.html',{'todos':todos})
