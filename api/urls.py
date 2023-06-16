61634c3b986240dea7e449d63cfc0b05829f8e76
christy <vennesach@gmail.com>
2023-06-16 18:20:13 +0800

    完成api撰寫

--- api/urls.py
+++ api/urls.py
@@ -20,5 +20,7 @@ from . import views
 
 urlpatterns = [
     path("todos/",views.todos_api,name='todos_api'),
+    path("todos/<int:id>",views.todo_api,name='todo_api'),
+    path("todos/user/<int:id>",views.user_todos_api,name='user_todos_api'),
 
 ]
